d7aa2d62c645a35c56b3981a04b083ec8cb1a69a
Halvor Vivelstad <halvorviv@gmail.com>
2022-10-12 21:26:49 +0200

    :sparkles: Add some motion

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -22,7 +22,22 @@ const ProjectsPage = () => {
             <div className="w-full mx-2 mb-5 text-2xl font-semibold">
               Projects
             </div>
-            <div className="flex flex-row justify-between">
+            <motion.ul
+              className="flex flex-row justify-between"
+              variants={{
+                hidden: { opacity: 1, scale: 0 },
+                visible: {
+                  opacity: 1,
+                  scale: 1,
+                  transition: {
+                    delayChildren: 0.15,
+                    staggerChildren: 0.1,
+                  },
+                },
+              }}
+              initial="hidden"
+              animate="visible"
+            >
               <ProjectTile title="学中文" />
               <ProjectTile title="学中文" />
               <ProjectTile title="学中文" />
@@ -31,22 +46,68 @@ const ProjectsPage = () => {
               <ProjectTile title="学中文" />
               <ProjectTile title="学中文" />
               <ProjectTile title="学中文" />
-            </div>
+            </motion.ul>
           </div>
 
-          <div className="pt-[10%] w-full px-[25%]">
-            <div className="mx-2 mb-5 text-2xl font-semibold">
+          <motion.div
+            className="pt-[10%] w-full px-[25%]"
+            initial="hidden"
+            animate="visible"
+            variants={{
+              visible: {
+                transition: {
+                  staggerChildren: 0.025,
+                },
+              },
+            }}
+          >
+            <motion.span
+              className="mx-2 mb-5 text-2xl font-semibold"
+              variants={{
+                hidden: {
+                  y: "200%",
+                  color: "transparent",
+                  transition: {
+                    ease: [0.455, 0.03, 0.515, 0.955],
+                    duration: 5,
+                  },
+                },
+                visible: {
+                  y: 0,
+                  color: "#FFFFFF",
+                  transition: {
+                    ease: [0.455, 0.03, 0.515, 0.955],
+                    duration: 0.75,
+                  },
+                },
+              }}
+            >
               Recommended blog posts
-            </div>
-            <div className="w-full grid grid-cols-3  ">
+            </motion.span>
+            <motion.ul
+              className="w-full grid grid-cols-3"
+              initial="hidden"
+              animate="visible"
+              variants={{
+                hidden: { opacity: 1, scale: 0 },
+                visible: {
+                  opacity: 1,
+                  scale: 1,
+                  transition: {
+                    delayChildren: 0.15,
+                    staggerChildren: 0.1,
+                  },
+                },
+              }}
+            >
               <BlogTile />
               <BlogTile />
               <BlogTile />
               <BlogTile />
               <BlogTile />
               <BlogTile />
-            </div>
-          </div>
+            </motion.ul>
+          </motion.div>
         </div>
       </SafariWindow>
     </motion.div>
@@ -55,7 +116,19 @@ const ProjectsPage = () => {
 
 function ProjectTile({ title }: { title: string }) {
   return (
-    <div className="flex flex-col mx-2">
+    <motion.li
+      className="flex flex-col mx-2"
+      whileHover={{
+        scale: 1.1,
+      }}
+      variants={{
+        hidden: { y: 20, opacity: 0 },
+        visible: {
+          y: 0,
+          opacity: 1,
+        },
+      }}
+    >
       <div className="rounded-md overflow-hidden bg-red-50 h-24 w-24">
         <Image
           src="https://images.unsplash.com/photo-1665510392901-2ca57b159f9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
@@ -68,13 +141,26 @@ function ProjectTile({ title }: { title: string }) {
       <div className="rounded-md overflow-clip w-24 h-5 text-center mt-1">
         {title}
       </div>
-    </div>
+    </motion.li>
   );
 }
 
 function BlogTile({}: {}) {
   return (
-    <div className="flex flex-row backdrop-blur-lg bg-white/30 p-5 rounded-xl m-2">
+    <motion.li
+      className="flex flex-row backdrop-blur-lg bg-white/30 p-5 rounded-xl m-2"
+      whileHover={{
+        scale: 1.05,
+        borderRadius: "10px",
+      }}
+      variants={{
+        hidden: { y: 20, opacity: 0 },
+        visible: {
+          y: 0,
+          opacity: 1,
+        },
+      }}
+    >
       <div className="flex flex-col">
         <div className="font-bold">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -92,7 +178,7 @@ function BlogTile({}: {}) {
           alt=""
         />
       </div>
-    </div>
+    </motion.li>
   );
 }
 
